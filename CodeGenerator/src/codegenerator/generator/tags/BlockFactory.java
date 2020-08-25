293306d71aeeac158eff036998a91de7b2dd9225
Wes Kaylor <oldgreydog@users.noreply.github.com>
2020-08-24 20:53:25 -0400

    Change the BlockFactory to speed up the parsing of templates.

--- CodeGenerator/src/codegenerator/generator/tags/BlockFactory.java
+++ CodeGenerator/src/codegenerator/generator/tags/BlockFactory.java
@@ -1,5 +1,5 @@
 /*
-	Copyright 2016 Wes Kaylor
+	Copyright 2020 Wes Kaylor
 
 	This file is part of CodeGenerator.
 
@@ -32,35 +32,37 @@ import java.util.*;
 public class BlockFactory {
 
 	// Data Members
-	protected static	LinkedList<TemplateBlock_Base>	m_blocks = new LinkedList<TemplateBlock_Base>();
+	protected static	TreeMap<String, TemplateBlock_Base>	m_blocks = new TreeMap<>();
 
 
-	// Each block class has to define in its constructor the tag name that it handles.  This class then uses that fact to find the target handler for the requested tag name.
+	// The block name is converted to lower case so that we can universally force the names searched for in GetBlock() below to lower case and thereby make it possible to handle whatever capitalization the user uses in their templates (i.e. camelcase, all lower, all cap, etc.).
 	static {
-		//m_blocks.add(new ConfigVariable());	// This class is a special case that has to be handled by the TextBlock.
-		m_blocks.add(new CamelCase());
-		m_blocks.add(new Counter());
-		m_blocks.add(new CustomCodeBlock());
-		m_blocks.add(new DecrementCounter());
-		m_blocks.add(new FileBlock());
-		m_blocks.add(new FirstElseBlock());
-		m_blocks.add(new FirstLetterToLowerCase());
-		m_blocks.add(new ForEachBlock());
+		//m_blocks.put(.BLOCK_NAME.toLowerCase(), new ConfigVariable());	// This class is a special case that has to be handled by the TextBlock.
+		m_blocks.put(CamelCase.BLOCK_NAME.toLowerCase(),				new CamelCase());
+		m_blocks.put(Counter.BLOCK_NAME.toLowerCase(),					new Counter());
+		m_blocks.put(CounterDecrement.BLOCK_NAME.toLowerCase(),			new CounterDecrement());
+		m_blocks.put(CounterIncrement.BLOCK_NAME.toLowerCase(),			new CounterIncrement());
+		m_blocks.put(CounterVariable.BLOCK_NAME.toLowerCase(),			new CounterVariable());
+		m_blocks.put(CustomCodeBlock.BLOCK_NAME.toLowerCase(),			new CustomCodeBlock());
+		m_blocks.put(FileBlock.BLOCK_NAME.toLowerCase(),				new FileBlock());
+		m_blocks.put(FirstElseBlock.BLOCK_NAME.toLowerCase(),			new FirstElseBlock());
+		m_blocks.put(FirstLetterToLowerCase.BLOCK_NAME.toLowerCase(),	new FirstLetterToLowerCase());
+		m_blocks.put(ForEachBlock.BLOCK_NAME.toLowerCase(),				new ForEachBlock());
 
-		m_blocks.add(new IfElseBlock());
-			m_blocks.add(new If_Boolean.And());	// The "AND", "OR" and "NOT" boolean logic tags for the IfElseBlock.
-			m_blocks.add(new If_Boolean.Or());
-			m_blocks.add(new If_Boolean.Not());
+		m_blocks.put(IfElseBlock.BLOCK_NAME.toLowerCase(),				new IfElseBlock());
+			m_blocks.put(If_Boolean.And.BLOCK_NAME.toLowerCase(),		new If_Boolean.And());	// The "AND", "OR" and "NOT" boolean logic tags for the IfElseBlock.
+			m_blocks.put(If_Boolean.Or.BLOCK_NAME.toLowerCase(),		new If_Boolean.Or());
+			m_blocks.put(If_Boolean.Not.BLOCK_NAME.toLowerCase(),		new If_Boolean.Not());
 
-		m_blocks.add(new OuterContext());
-		m_blocks.add(new OuterContextEval());
-		m_blocks.add(new TabMarker());
-		m_blocks.add(new TabSettings());
-		m_blocks.add(new TabStop());
-		m_blocks.add(new TextBlock());
-		m_blocks.add(new TypeConvert());
-		m_blocks.add(new TypeConvertLoadFile());
-		m_blocks.add(new VariableBlock());
+		m_blocks.put(OuterContext.BLOCK_NAME.toLowerCase(),				new OuterContext());
+		m_blocks.put(OuterContextEval.BLOCK_NAME.toLowerCase(),			new OuterContextEval());
+		m_blocks.put(TabMarker.BLOCK_NAME.toLowerCase(),				new TabMarker());
+		m_blocks.put(TabSettings.BLOCK_NAME.toLowerCase(),				new TabSettings());
+		m_blocks.put(TabStop.BLOCK_NAME.toLowerCase(),					new TabStop());
+		m_blocks.put(TextBlock.BLOCK_NAME.toLowerCase(),				new TextBlock());
+		m_blocks.put(TypeConvert.BLOCK_NAME.toLowerCase(),				new TypeConvert());
+		m_blocks.put(TypeConvertLoadFile.BLOCK_NAME.toLowerCase(),		new TypeConvertLoadFile());
+		m_blocks.put(VariableBlock.BLOCK_NAME.toLowerCase(),			new VariableBlock());
 	}
 
 
@@ -72,10 +74,9 @@ public class BlockFactory {
 	 * @return			Returns NULL if the tag name isn't defined by any of the contained classes.
 	 */
 	public static TemplateBlock_Base GetBlock(String p_tagName) {
-		for (TemplateBlock_Base t_nextBlock: m_blocks) {
-			if (t_nextBlock.GetName().equalsIgnoreCase(p_tagName))
-				return t_nextBlock.GetInstance();
-		}
+		TemplateBlock_Base t_sourceBlock = m_blocks.get(p_tagName.toLowerCase());	// As noted above, forcing everything to lower case makes it possible to handle whatever capitalization scheme is used in the template.
+		if (t_sourceBlock != null)
+			return t_sourceBlock.GetInstance();
 
 		return null;
 	}
