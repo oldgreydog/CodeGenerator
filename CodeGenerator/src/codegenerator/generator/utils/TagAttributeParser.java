79104d4d279db9e1e6f5e21d6e749d94a0b97fa7
Wes Kaylor <oldgreydog@users.noreply.github.com>
2021-11-11 23:41:25 -0500

    Back when I changed the tag attribute parser to allow embedded tags, I didn't quite get it right since it required double quotes around tag- embedded names/values even though they didn't have spaces in them. That's fixed now.  Double quotes should only be required now if you have any white space in the name/value.

--- CodeGenerator/src/codegenerator/generator/utils/TagAttributeParser.java
+++ CodeGenerator/src/codegenerator/generator/utils/TagAttributeParser.java
@@ -24,6 +24,8 @@ package codegenerator.generator.utils;
 
 import coreutil.logging.*;
 
+import java.util.*;
+
 import codegenerator.generator.tags.*;
 import codegenerator.generator.utils.TemplateTokenizer.*;
 
@@ -38,30 +40,45 @@ import codegenerator.generator.utils.TemplateTokenizer.*;
 public class TagAttributeParser {
 
 	// Data members
-	private TemplateBlock_Base		m_attributeName;
-	private TemplateBlock_Base		m_value;
-	private int						m_lineNumber		= -1;
+	private enum ParseState {START_NAME,
+							 IN_NAME,
+							 EXPECT_EQUALS,
+							 START_VALUE,
+							 IN_VALUE
+							 }
+
+	private GeneralBlock		m_attributeName;
+	private GeneralBlock		m_value;
+	private int					m_lineNumber		= -1;
 
 
 	//*********************************
-	public TemplateBlock_Base GetAttributeName() {
+	public GeneralBlock GetAttributeName() {
 		return m_attributeName;
 	}
 
 
 	//*********************************
 	/**
-	 * Converting the parser to have more flexible name and value typing of Tag_Base instead of Text lead to
+	 * Converting the parser to have more flexible name and value typing of GeneralBlock instead of Text lead to
 	 * adding this helper function that will let most classes that only need plain text attributes to get those values without
 	 * having to do the conversions themselves.  Of course, that means that if the either of the attributes values is changed to
 	 * be a more complex type, then they have to handle a NULL return from this and then check to see if a more complex type is available.
 	 *
-	 * @return Can be NULL.
+	 * @return Can be NULL if the value is missing or if the value contains tags that can only have a value during the evaluation phase of execution.
 	 */
 	public String GetAttributeNameAsString() {
 		try {
-			if ((m_attributeName != null) && (m_attributeName.GetName() == TextBlock.BLOCK_NAME))
-				return  ((TextBlock)m_attributeName).GetText();
+			if (m_attributeName == null)
+				return null;
+
+			LinkedList<Tag_Base> t_attributeTags = m_attributeName.GetChildNodeList();
+			if ((t_attributeTags == null) || (t_attributeTags.isEmpty() || (t_attributeTags.size() > 1)))
+				return null;
+
+			Tag_Base t_onlyTag = t_attributeTags.getFirst();
+			if (t_onlyTag.GetName() == Text.TAG_NAME)
+				return  ((Text)t_onlyTag).GetText();
 
 			return null;
 		}
@@ -73,24 +90,32 @@ public class TagAttributeParser {
 
 
 	//*********************************
-	public TemplateBlock_Base GetAttributeValue() {
+	public GeneralBlock GetAttributeValue() {
 		return m_value;
 	}
 
 
 	//*********************************
 	/**
-	 * Converting the parser to have more flexible name and value typing of TemplateBlock_Base instead of TextBlock lead to
+	 * Converting the parser to have more flexible name and value typing of GeneralBlock instead of Text lead to
 	 * adding this helper function that will let most classes that only need plain text attributes to get those values without
 	 * having to do the conversions themselves.  Of course, that means that if the either of the attributes values is changed to
 	 * be a more complex type, then they have to handle a NULL return from this and then check to see if a more complex type is available.
 	 *
-	 * @return Can be NULL if the object type is not TextBlock.
+	 * @return Can be NULL if the value is missing or if the value contains tags that can only have a value during the evaluation phase of execution.
 	 */
 	public String GetAttributeValueAsString() {
 		try {
-			if ((m_value != null) && (m_value.GetName() == TextBlock.BLOCK_NAME))
-				return  ((TextBlock)m_value).GetText();
+			if (m_value == null)
+				return null;
+
+			LinkedList<Tag_Base> t_attributeTags = m_value.GetChildNodeList();
+			if ((t_attributeTags == null) || (t_attributeTags.isEmpty() || (t_attributeTags.size() > 1)))
+				return null;
+
+			Tag_Base t_onlyTag = t_attributeTags.getFirst();
+			if (t_onlyTag.GetName() == Text.TAG_NAME)
+				return  ((Text)t_onlyTag).GetText();
 
 			return null;
 		}
@@ -112,18 +137,18 @@ public class TagAttributeParser {
 		try {
 			m_lineNumber = p_tokenizer.GetLineCount();
 
-			// A general block will parse child tags until it finds a tag that isn't a command.  That tag should be the closing tag for the parent block.
-			Token				t_nextToken;
-			TagParser			t_tagParser;
-			Tag_Base			t_newTag;
-			Text				t_text;
+			Token		t_nextToken;
+			TagParser	t_tagParser;
+			Tag_Base	t_newTag;
+			Text		t_text;
+			ParseState	t_parseState = ParseState.START_NAME;
 
 			while ((t_nextToken = p_tokenizer.GetNextToken()) != null) {
 				switch (t_nextToken.m_tokenType) {
 					case Token.TOKEN_TYPE_OPENING_DELIMITER:
 						t_tagParser = new TagParser();
 						if (!t_tagParser.Parse(p_tokenizer)) {
-							Logger.LogError("TextBlock.Parse() failed to parse the tag at line [" + p_tokenizer.GetLineCount() + "].");
+							Logger.LogError("TagAttributeParser.Parse() failed to parse the tag at line [" + p_tokenizer.GetLineCount() + "].");
 							return false;
 						}
 
@@ -136,11 +161,28 @@ public class TagAttributeParser {
 								return false;
 							}
 
-							if (m_attributeName == null)
-								m_attributeName = t_configVariable;
-							else {
-								m_value = t_configVariable;
-								return true;
+							switch (t_parseState) {
+								case START_NAME:
+									t_parseState = ParseState.IN_NAME;	// Set the new state and fall through to the next case to add the tag to the child node list.
+								case IN_NAME:
+									if (m_attributeName == null)
+										m_attributeName = new GeneralBlock();
+
+									m_attributeName.AddChildNode(t_configVariable);
+									break;
+
+								case EXPECT_EQUALS:
+									Logger.LogError("TagAttributeParser.Parse() is expecting the equals sign but found config variable at line [" + p_tokenizer.GetLineCount() + "].");
+									return false;
+
+								case START_VALUE:
+									t_parseState = ParseState.IN_VALUE;	// Set the new state and fall through to the next case to add the tag to the child node list.
+								case IN_VALUE:
+									if (m_value == null)
+										m_value = new GeneralBlock();
+
+									m_value.AddChildNode(t_configVariable);
+									break;
 							}
 
 							break;
@@ -159,11 +201,28 @@ public class TagAttributeParser {
 									return false;
 								}
 
-								if (m_attributeName == null)
-									m_attributeName = t_newTag;
-								else {
-									m_value = t_newTag;
-									return true;
+								switch (t_parseState) {
+									case START_NAME:
+										t_parseState = ParseState.IN_NAME;	// Set the new state and fall through to the next case to add the tag to the child node list.
+									case IN_NAME:
+										if (m_attributeName == null)
+											m_attributeName = new GeneralBlock();
+
+										m_attributeName.AddChildNode(t_newTag);
+										break;
+
+									case EXPECT_EQUALS:
+										Logger.LogError("TagAttributeParser.Parse() is expecting the tag [" + t_newTag.GetName() + "] at line [" + p_tokenizer.GetLineCount() + "].");
+										return false;
+
+									case START_VALUE:
+										t_parseState = ParseState.IN_VALUE;	// Set the new state and fall through to the next case to add the tag to the child node list.
+									case IN_VALUE:
+										if (m_value == null)
+											m_value = new GeneralBlock();
+
+										m_value.AddChildNode(t_newTag);
+										break;
 								}
 							}
 							else {
@@ -185,9 +244,42 @@ public class TagAttributeParser {
 						return true;
 
 					case Token.TOKEN_TYPE_EQUALS:
+						switch (t_parseState) {
+							case START_NAME:
+								Logger.LogError("TagAttributeParser.Parse() found an equals sign at line [" + p_tokenizer.GetLineCount() + "] but it was expecting to find the attribute name.");
+								return false;
+
+							case IN_NAME:
+							case EXPECT_EQUALS:
+								t_parseState = ParseState.START_VALUE;
+								break;
+
+							case START_VALUE:
+							case IN_VALUE:
+								Logger.LogError("TagAttributeParser.Parse() found an equals sign at line [" + p_tokenizer.GetLineCount() + "] but it was expecting to find the attribute value.");
+								return false;
+						}
+
 						break;
 
 					case Token.TOKEN_TYPE_WHITE_SPACE:
+
+						switch (t_parseState) {
+							case START_NAME:
+								break;	// Just eat any white space before the name.
+							case IN_NAME:
+								t_parseState = ParseState.EXPECT_EQUALS;
+								break;
+
+							case EXPECT_EQUALS:
+								break;	// We can eat any white space between the name and the equals sign.
+
+							case START_VALUE:
+								break;	// Just eat any white space between the equals and the value.
+							case IN_VALUE:
+								return true;	// We're done with the attribute value and, therefore, the attribute, so it's time to return.
+						}
+
 						break;
 
 					case Token.TOKEN_TYPE_WORD:
@@ -195,11 +287,31 @@ public class TagAttributeParser {
 						t_text = new Text();
 						t_text.SetText(t_nextToken.m_tokenValue);
 
-						if (m_attributeName == null)
-							m_attributeName = t_text;
-						else {
-							m_value = t_text;
-							return true;
+						switch (t_parseState) {
+							case START_NAME:
+								t_parseState = ParseState.IN_NAME;
+							case IN_NAME:
+								if (m_attributeName == null)
+									m_attributeName = new GeneralBlock();
+
+								m_attributeName.AddChildNode(t_text);
+
+								// We can't change the state to EXPECT_EQUALS here because this may be a mixed value (i.e. text + tags).
+								break;
+
+							case EXPECT_EQUALS:
+								break;	// We can eat any white space between the name and the equals sign.
+
+							case START_VALUE:
+								t_parseState = ParseState.IN_VALUE;
+							case IN_VALUE:
+								if (m_value == null)
+									m_value = new GeneralBlock();
+
+								m_value.AddChildNode(t_text);
+
+								// We can't return here because this may be a mixed value (i.e. text + tags).
+								break;
 						}
 
 						break;
@@ -214,11 +326,29 @@ public class TagAttributeParser {
 							return false;
 						}
 
-						if (m_attributeName == null)
-							m_attributeName = t_text;
-						else {
-							m_value = t_text;
-							return true;
+						switch (t_parseState) {
+							case START_NAME:
+							case IN_NAME:
+								if (m_attributeName == null)
+									m_attributeName = new GeneralBlock();
+
+								m_attributeName.AddChildNode(t_text);
+
+								t_parseState = ParseState.EXPECT_EQUALS;	// Since this was a double-quoted value, we are done with the name and now expect the equals sign.
+								break;
+
+							case EXPECT_EQUALS:
+								Logger.LogError("TagAttributeParser.Parse() is expecting the equals sign at line [" + p_tokenizer.GetLineCount() + "] but found the start of a double-quoted value.");
+								return false;
+
+							case START_VALUE:
+							case IN_VALUE:
+								if (m_value == null)
+									m_value = new GeneralBlock();
+
+								m_value.AddChildNode(t_text);
+
+								return true;	// Since this was a double-quoted value, we are done with the attribute value and can return.
 						}
 
 						break;
