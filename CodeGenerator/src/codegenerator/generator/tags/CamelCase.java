00b70f589252d0fdd8c7977730563647520a459a
Wes Kaylor <helots54612@mypacks.net>
2024-06-29 15:39:56 -0400

    Add an optional output separator attribute and change the name of the input separator attribute (but leave the old name in the code so that it's still backwards compatible.

--- CodeGenerator/src/codegenerator/generator/tags/CamelCase.java
+++ CodeGenerator/src/codegenerator/generator/tags/CamelCase.java
@@ -47,15 +47,18 @@ characters between the words so that, for example, it can be used for a UI label
 */
 public class CamelCase extends Tag_Base {
 
-	static public final String		TAG_NAME						= "camelCase";
+	static public final String		TAG_NAME								= "camelCase";
 
-	static private final String		ATTRIBUTE_VALUE					= "value";
-	static private final String		ATTRIBUTE_OPTIONAL_SEPARATOR	= "optionalSeparator";
+	static private final String		ATTRIBUTE_VALUE							= "value";
+	static private final String		OLD_ATTRIBUTE_OPTIONAL_SEPARATOR		= "optionalSeparator";			// I'm leaving this attribute name in here so that I don't have to bump the major version number with a completely breaking change with the new attribute names.
+	static private final String		ATTRIBUTE_OPTIONAL_INPUT_SEPARATOR		= "optionalInputSeparator";
+	static private final String		ATTRIBUTE_OPTIONAL_OUTPUT_SEPARATOR		= "optionalOutputSeparator";
 
 
 	// Data members
-	private	Tag_Base		m_value					= null;
-	private	String			m_optionalSeparator		= null;
+	private	Tag_Base		m_value						= null;
+	private	String			m_inputSeparator			= "_";		// This is just a default value that is about 100% of my usage.
+	private	String			m_optionalOutputSeparator	= null;
 
 
 	//*********************************
@@ -87,11 +90,30 @@ public class CamelCase extends Tag_Base {
 				return false;
 			}
 
-			t_nodeAttribute = p_tagParser.GetNamedAttribute(ATTRIBUTE_OPTIONAL_SEPARATOR);
+			boolean t_foundOldAttributeName = false;
+			t_nodeAttribute = p_tagParser.GetNamedAttribute(ATTRIBUTE_OPTIONAL_INPUT_SEPARATOR);
+			if (t_nodeAttribute == null)  {
+				t_nodeAttribute = p_tagParser.GetNamedAttribute(OLD_ATTRIBUTE_OPTIONAL_SEPARATOR);
+				if (t_nodeAttribute != null) {
+					Logger.LogWarning("CamelCase.Init() found the old optional input attribute name [" + OLD_ATTRIBUTE_OPTIONAL_SEPARATOR + "] in the CamelCase tag at line number [" + m_lineNumber + "].  This will not affect the tag for now, but that attribute name should be replaced with the new name [" + ATTRIBUTE_OPTIONAL_INPUT_SEPARATOR + "] where possible.");
+					t_foundOldAttributeName = true;
+				}
+			}
+
+			// Since the input separator attribute is optional, we may get here without it being found so we have to check for it no matter what.
 			if (t_nodeAttribute != null) {
-				m_optionalSeparator = t_nodeAttribute.GetAttributeValueAsString();
-				if (m_optionalSeparator == null) {
-					Logger.LogError("CamelCase.Init() did not get the value from the optional attribute [" + ATTRIBUTE_OPTIONAL_SEPARATOR + "] at line number [" + m_lineNumber + "].");
+				m_inputSeparator = t_nodeAttribute.GetAttributeValueAsString();
+				if ((m_inputSeparator == null) || m_inputSeparator.isBlank()) {
+					Logger.LogError("CamelCase.Init() did not get the value from the optional attribute [" + (t_foundOldAttributeName ? OLD_ATTRIBUTE_OPTIONAL_SEPARATOR : ATTRIBUTE_OPTIONAL_INPUT_SEPARATOR) + "] at line number [" + m_lineNumber + "].");
+					return false;
+				}
+			}
+
+			t_nodeAttribute = p_tagParser.GetNamedAttribute(ATTRIBUTE_OPTIONAL_OUTPUT_SEPARATOR);
+			if (t_nodeAttribute != null) {
+				m_optionalOutputSeparator = t_nodeAttribute.GetAttributeValueAsString();
+				if ((m_optionalOutputSeparator == null) || m_optionalOutputSeparator.isBlank()) {
+					Logger.LogError("CamelCase.Init() did not get the value from the optional attribute [" + ATTRIBUTE_OPTIONAL_OUTPUT_SEPARATOR + "] at line number [" + m_lineNumber + "].");
 					return false;
 				}
 			}
@@ -142,7 +164,15 @@ public class CamelCase extends Tag_Base {
 
 			p_evaluationContext.PopCurrentCursor();	// Get rid of the temp cursor because we can't leave it on the context when we leave.
 
-			p_evaluationContext.GetCursor().Write(CreateCamelCaseName(t_valueWriter.toString()));
+			String t_result = CreateCamelCaseValue(t_valueWriter.toString());
+			if ((t_result == null) || t_result.isBlank()) {
+				Logger.LogError("CamelCase.Evaluate() failed to evaluate the value at line number [" + m_lineNumber + "].");
+				p_evaluationContext.PopCurrentCursor();	// We need to clean up the temp cursor before we fail out of the function.
+				return false;
+			}
+
+
+			p_evaluationContext.GetCursor().Write(t_result);
 		}
 		catch (Throwable t_error) {
 			Logger.LogException("CamelCase.Evaluate() failed with error: ", t_error);
@@ -171,53 +201,54 @@ public class CamelCase extends Tag_Base {
 
 
 	//*********************************
-	private String CreateCamelCaseName(String p_className) {
-		StringBuilder t_newValue = new StringBuilder();
+	private String CreateCamelCaseValue(String p_value) {
+		try {
+			StringBuilder t_newValue = new StringBuilder();
 
-		if (p_className == null) {
-			Logger.LogError("CamelCase.CreateCamelCaseName(): A NULL was passed in.");
-			return null;
-		}
+			if ((p_value == null) || p_value.isBlank()) {
+				Logger.LogError("CamelCase.CreateCamelCaseValue(): A NULL or empty value was passed in.");
+				return null;
+			}
 
-		String	t_separator		= "_";
-		char	t_separatorChar	= '_';
-		if (p_className.contains("-")) {
-			t_separator		= "-";
-			t_separatorChar	= '-';
-		}
+			// If this name doesn't contain any separators, then we'll just lower-case the rest of the name, add it on and return it.
+			if (!p_value.contains(m_inputSeparator)) {
+				t_newValue.append(p_value.substring(0, 1).toUpperCase() + p_value.substring(1).toLowerCase());
+				return t_newValue.toString();
+			}
 
-		if (!p_className.contains(t_separator)) {	// If this name doesn't contain any separators, then we'll just lower-case the rest of the name, add it on and return it.
-			t_newValue.append(p_className.substring(0, 1).toUpperCase() + p_className.substring(1).toLowerCase());
+			// Otherwise, we need to create a camel-case of the name and remove the input separator.
+			String	t_remainder		= p_value;
+			boolean	t_firstAdded	= false;
+			int		t_index;
+
+			// This loop will only go to the last instance of the input separator, so there will be a (possibly) dangling final part of the string that will have to be handled by the code after this loop below.
+			while (t_remainder.contains(m_inputSeparator)) {
+				if (t_firstAdded) {
+					if (m_optionalOutputSeparator != null)
+						t_newValue.append(m_optionalOutputSeparator);
+				}
+				else
+					t_firstAdded = true;
+
+				t_index = t_remainder.indexOf(m_inputSeparator);
+				t_newValue.append(t_remainder.substring(0, 1).toUpperCase() + t_remainder.substring(1, t_index).toLowerCase());
+				t_remainder = t_remainder.substring(++t_index);
+			}
+
+			// Only add the optional separator if there has already been at least one append on the new value.
+			if (t_firstAdded && (m_optionalOutputSeparator != null)) {
+					t_newValue.append(m_optionalOutputSeparator);
+			}
+
+			t_newValue.append(t_remainder.substring(0, 1).toUpperCase());
+			if (t_remainder.length() > 1)
+				t_newValue.append(t_remainder.substring(1).toLowerCase());
+
 			return t_newValue.toString();
 		}
-
-		// Otherwise, we need to create a camel-case of the name and remove the underscores.
-		String	t_remainder		= p_className;
-		boolean	t_firstAdded	= false;
-		int		t_index;
-		while (t_remainder.contains(t_separator)) {
-			if (t_firstAdded) {
-				if (m_optionalSeparator != null)
-					t_newValue.append(m_optionalSeparator);
-			}
-			else
-				t_firstAdded = true;
-
-			t_index = t_remainder.indexOf(t_separatorChar);
-			t_newValue.append(t_remainder.substring(0, 1).toUpperCase() + t_remainder.substring(1, t_index).toLowerCase());
-			t_remainder = t_remainder.substring(++t_index);
+		catch (Throwable t_error) {
+			Logger.LogException("CamelCase.CreateCamelCaseValue() failed with error: ", t_error);
+			return null;
 		}
-
-		// Only add the optional separator if there has already been at least one append on the new value.
-		if (t_firstAdded) {
-			if (m_optionalSeparator != null)
-				t_newValue.append(m_optionalSeparator);
-		}
-
-		t_newValue.append(t_remainder.substring(0, 1).toUpperCase());
-		if (t_remainder.length() > 1)
-			t_newValue.append(t_remainder.substring(1).toLowerCase());
-
-		return t_newValue.toString();
 	}
 }
